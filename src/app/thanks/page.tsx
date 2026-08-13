4dab820cb74988de825acde1daa0b93d56ff63e7
mio <mio.1016.3030@gmail.com>
2026-08-14 00:37:19 +0900

    Improve thanks page line breaks

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -28,16 +28,15 @@ export default function ThanksPage() {
           <span />
         </div>
         <p className="thanks-lead">
-          あなたの空間に、まだ誰も見たことのない光を灯す準備が始まりました。
-          <br />
-          いただいたメッセージは、私たちのスクリーンにしっかり投影されました。
+          <span>あなたの空間に、</span>
+          <span>まだ誰も見たことのない光を灯す準備が始まりました。</span>
+          <span>いただいたメッセージは、</span>
+          <span>私たちのスクリーンにしっかり投影されました。</span>
         </p>
         <p className="thanks-copy">
-          内容を拝見し、2営業日以内にご連絡いたします。
-          <br />
-          どんな空間が、どんな物語を待っているのか、
-          <br />
-          私たちも今からワクワクしています。
+          <span>内容を拝見し、2営業日以内にご連絡いたします。</span>
+          <span>どんな空間が、どんな物語を待っているのか、</span>
+          <span>私たちも今からワクワクしています。</span>
         </p>
         <div className="thanks-actions">
           <Link className="thanks-primary" href="/">
